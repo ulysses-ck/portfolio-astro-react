f298e43d0fbaf79210bb32151ca1c6c6c281d690
ulysses-ck <ulisesapaza.280@gmail.com>
2024-04-05 00:52:18 -0300

    fix: bad rules of eslint and astro

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,25 +5,34 @@ module.exports = {
   },
   extends: [
     "standard-with-typescript",
-    "plugin:react/recommended",
     "plugin:sonarjs/recommended",
     "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
   ],
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
       parserOptions: {
-        sourceType: "script",
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
       },
     },
   ],
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: [],
   rules: {},
 };
